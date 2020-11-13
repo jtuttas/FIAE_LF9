fa6708ad3d500a0703a3f86399c2ca8d78b513c6
jtuttas <jtuttas@gmx.net>
2020-11-13 16:57:02 +0100

    mit App

---
+++ src/main/java/schueler/App.java
@@ -0,0 +1,14 @@
+package schueler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.sqlite.SQLiteConfig;
+
+public class App {
+    
+    public static void main(String[] args) {
+    }
+}
