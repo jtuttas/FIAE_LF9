ac6ce086757a3b0e132a6b64dd8c105df49ddca8
jtuttas <jtuttas@gmx.net>
2022-12-08 09:10:26 +0100

    App angepasst

--- src/main/java/schueler/App.java
+++ src/main/java/schueler/App.java
@@ -10,6 +10,21 @@ import org.sqlite.SQLiteConfig;
 public class App {
     
     public static void main(String[] args) {
-        System.out.println("Hallo Welt!!");
+        try {
+            Class.forName("org.sqlite.JDBC");
+            SQLiteConfig config = new SQLiteConfig();
+            config.enforceForeignKeys(true);
+            Connection c = DriverManager.getConnection("jdbc:sqlite:todo_klein_org.db", config.toProperties());
+            Statement st = c.createStatement();
+            ResultSet rs = st.executeQuery("select * from priority");
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + " " + rs.getInt(2) + " " + rs.getString(3));
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Treiber nicht gefunden");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
